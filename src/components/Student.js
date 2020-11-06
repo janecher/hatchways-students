f62e56a87824d8d73fb621224b4af9fd3ec3e96b
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-11-06 11:11:16 -0800

    add Student component

---
+++ src/components/Student.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import PropTypes from "prop-types";
+
+function Student(props){
+
+  const {img, firstName, lastName, email, company, skill, grades} = props;
+
+  const [showGradesList, setShowGradesList] = useState(false);
+
+  const average = (array) =>{
+    let average = 0;
+    for(let i=0; i<array.length; i++){
+      average +=array[i];
+    }
+    if(array.length != 0)
+    {
+      average /= array.length;
+    }
+    return average;
+  }
+
+  let gradeList = null;
+
+  // if(showGradesList){
+  //   currentPage = <GradeList grades={grades} />
+  // } 
+
+  return (
+    <React.Fragment>
+      <div className={}>
+        <img src={img} alt="Profile picture"/>
+        <h3>{firstName} {lastName}</h3>
+        <p>Email: {email}</p>
+        <p>Company: {company}</p>
+        <p>Skill: {skill}</p>
+        <p>Average: {average(grades)}</p>
+      </div>
+      <div>
+        {gradeList}
+      </div>
+    </React.Fragment>
+  );
+}
+
+Student.propTypes = {
+  img: PropTypes.string, 
+  firstName: PropTypes.string, 
+  lastName: PropTypes.string, 
+  email: PropTypes.string, 
+  company: PropTypes.string, 
+  skill: PropTypes.string, 
+  grades: PropTypes.array
+};
+
+export default Student
